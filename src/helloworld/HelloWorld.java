7e10ca296aa0571c2d4148cef4d13f94e120c708
AleksiJ <a@gmail.com>
2016-03-30 13:51:39 +0300

    Juho ei osaa 3.0

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -17,6 +17,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Lisätty jotain hauskaa");
+        System.out.println("Juho ei osaa");
     }
     
 }
